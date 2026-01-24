8310d8eff365fa35dfee94efab3bd4f0bd451583
Nick Stanley <nick.stanley@backstopsolutions.com>
2026-01-24 08:57:49 -0600

    DEVOPS-10383-nstanley

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 locals {
   component                         = "internal"
-  region_datacenter                 = "${data.aws_region.current.name}-${var.cluster_datacenter}"
+  region_datacenter                 = "${data.aws_region.current.id}-${var.cluster_datacenter}"
   final_parameter_group_name        = var.parameter_group_name == "" ? "${var.cluster_id}-${local.region_datacenter}" : var.parameter_group_name
   final_parameter_group_description = var.parameter_group_description == "" ? "Managed by Terraform" : "Managed by Terraform ${var.parameter_group_description}"
   account_id                        = data.aws_caller_identity.current.account_id
