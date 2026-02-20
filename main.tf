03a7ccecc810effcc8c7e64fcb6d6e20804093e7
Nick Stanley <nick.stanley@backstopsolutions.com>
2026-02-20 10:22:05 -0600

    DEVOPS-10383

--- main.tf
+++ main.tf
@@ -214,10 +214,11 @@ resource "aws_elasticache_user" "default" {
 resource "aws_elasticache_user_group" "cluster_user_group" {
   engine        = "redis"
   user_group_id = "${var.cluster_datacenter}-${var.cluster_id}-usergrp"
-  user_ids = concat(
-    [aws_elasticache_user.default.user_id],
-    # [for user in aws_elasticache_user.runtime : user.user_id]
-  )
+  user_ids = [aws_elasticache_user.default.user_id]
+  # user_ids = concat(
+  #   [aws_elasticache_user.default.user_id],
+  #   # [for user in aws_elasticache_user.runtime : user.user_id]
+  # )
 }
 
 
